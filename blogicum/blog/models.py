98c04e56213d165ec88f89bdc2e7a1cf146817ba
Andrey Murashov <amourashov@yandex.ru>
2023-11-18 19:22:48 +0300

    перед первым ревью

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -70,8 +70,10 @@ class Post(models.Model):
     )
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
-                                       help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+                                       help_text='Снимите галочку, '
+                                                 'чтобы скрыть публикацию.')
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Добавлено')
 
     class Meta:
         verbose_name = 'публикация'
